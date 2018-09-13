24d1e62dfe46fc7e7f0846e0755a18c7b8f91ce3
Bobby Boyd <bboyd2008@gmail.com>
2018-09-13 16:41:46 -0700

    added test for header tags

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,60 @@
-'use strict';
+"use strict";
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
+import * as vscode from "vscode";
+var fs = require("fs");
+var path = require("path");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.commands.registerCommand("extension.sayHello", () => {
+    const { activeTextEditor } = vscode.window;
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vs-org" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    if (
+      activeTextEditor &&
+      activeTextEditor.document.languageId === "markdown"
+    ) {
+      //active text editor
+      const { document } = activeTextEditor;
+      //get the current line
+      let setCurrentLine = activeTextEditor.selection.active.line;
+      const getCurrentLine = document.lineAt(setCurrentLine);
+      //get the text of the current line
+      let currentLineText = getCurrentLine.text;
+      //remove special characters
+      let formattedText = currentLineText.replace(/[^a-zA-Z0-9]/g, "");
+      //set the tag
+      let tag = getTag();
+      function getTag() {
+        if (currentLineText.indexOf("#") !== -1) {
+          for (var i = 0; i < currentLineText.length; i++) {
+            if (currentLineText[i + 3] === "#") {
+              return "h4";
+            }
+            if (currentLineText[i + 2] === "#") {
+              return "h3";
+            }
+            if (currentLineText[i + 1] === "#") {
+              return "h2";
+            }
+            return "h1";
+          }
+        }
+      }
+      //h1
+      if (tag === "h1") {
+        const edit = new vscode.WorkspaceEdit();
+        edit.delete(document.uri, getCurrentLine.range);
+        edit.insert(
+          document.uri,
+          getCurrentLine.range.start,
+          "\u233e" + formattedText
+        );
+        return vscode.workspace.applyEdit(edit);
+      }
+    }
+  });
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
