8fcfcd05ef687d4e1626eca046436134e02d8112
Bobby Boyd <bboyd2008@gmail.com>
2018-09-14 13:06:09 -0700

    basic vso support

--- src/extension.ts
+++ src/extension.ts
@@ -9,13 +9,10 @@ var path = require("path");
 export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand("extension.sayHello", () => {
     const { activeTextEditor } = vscode.window;
-
-    if (
-      activeTextEditor &&
-      activeTextEditor.document.languageId === "markdown"
-    ) {
+    if (activeTextEditor && activeTextEditor.document.languageId === "vso") {
       //active text editor
       const { document } = activeTextEditor;
+
       //get the current line
       let setCurrentLine = activeTextEditor.selection.active.line;
       const getCurrentLine = document.lineAt(setCurrentLine);
@@ -48,6 +45,7 @@ export function activate(context: vscode.ExtensionContext) {
           }
         }
       }
+
       //turn back into markdown
       if (tag === "\u233e") {
         const edit = new vscode.WorkspaceEdit();
@@ -61,6 +59,17 @@ export function activate(context: vscode.ExtensionContext) {
       }
       //h1
       if (tag === "h1") {
+        const edit = new vscode.WorkspaceEdit();
+        edit.delete(document.uri, getCurrentLine.range);
+        edit.insert(
+          document.uri,
+          getCurrentLine.range.start,
+          "\u2300" + formattedText
+        );
+        return vscode.workspace.applyEdit(edit);
+      }
+      //h2
+      if (tag === "h2") {
         const edit = new vscode.WorkspaceEdit();
         edit.delete(document.uri, getCurrentLine.range);
         edit.insert(
