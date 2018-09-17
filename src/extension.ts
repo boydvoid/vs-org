8564d24eb8de075368118057859e138cbdf063e2
Bobby Boyd <bboyd2008@gmail.com>
2018-09-17 15:33:28 -0700

    added TODO keyword to headings

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { format } from "path";
 
 const GO_MODE: vscode.DocumentFilter = { language: "vso", scheme: "file" };
 // this method is called when your extension is activated
@@ -22,7 +23,7 @@ class GoOnTypingFormatter implements vscode.OnTypeFormattingEditProvider {
             if (currentLine.text === "* ") {
               const getRange = document.lineAt(position).range;
               let removeText = vscode.TextEdit.delete(getRange);
-              let insertText = vscode.TextEdit.insert(position, "⊖ ");
+              let insertText = vscode.TextEdit.insert(position, "⊖");
               resolve([removeText, insertText]);
             }
           }
@@ -30,7 +31,7 @@ class GoOnTypingFormatter implements vscode.OnTypeFormattingEditProvider {
             if (currentLine.text === "** ") {
               const getRange = document.lineAt(position).range;
               let removeText = vscode.TextEdit.delete(getRange);
-              let insertText = vscode.TextEdit.insert(position, "  ⊙ ");
+              let insertText = vscode.TextEdit.insert(position, " ⊙");
               resolve([removeText, insertText]);
             }
           }
@@ -40,7 +41,7 @@ class GoOnTypingFormatter implements vscode.OnTypeFormattingEditProvider {
             if (currentLine.text.includes("*** ")) {
               const getRange = document.lineAt(position).range;
               let removeText = vscode.TextEdit.delete(getRange);
-              let insertText = vscode.TextEdit.insert(position, "    ✪ ");
+              let insertText = vscode.TextEdit.insert(position, "   ✪");
               resolve([removeText, insertText]);
             }
           }
@@ -58,4 +59,45 @@ export function activate(ctx: vscode.ExtensionContext): void {
       " "
     )
   );
+
+  //commands
+  vscode.commands.registerCommand("extension.addTodo", () => {
+    const { activeTextEditor } = vscode.window;
+
+    if (activeTextEditor && activeTextEditor.document.languageId === "vso") {
+      //active text editor
+      const { document } = activeTextEditor;
+      //get the current line
+      let position = activeTextEditor.selection.active.line;
+      const getCurrentLine = document.lineAt(position);
+      //get the text of the current line
+      let currentLineText = getCurrentLine.text;
+      //remove leading spaces
+      let trimmedText = currentLineText.trim();
+      //remove special characters
+      let formattedText = trimmedText.replace(/[^\w\s!?]/g, "");
+      console.log(formattedText);
+      //set the tag
+      addTodo("⊖");
+      addTodo(" ⊙");
+      addTodo("   ✪");
+      function addTodo(char: string) {
+        //need to see if there is a header character
+        //add TODO after header character and before text
+
+        if (currentLineText.includes(char)) {
+          let edit = new vscode.WorkspaceEdit();
+          edit.delete(document.uri, getCurrentLine.range);
+
+          edit.insert(
+            document.uri,
+            getCurrentLine.range.start,
+            char + " TODO " + formattedText
+          );
+
+          return vscode.workspace.applyEdit(edit);
+        }
+      }
+    }
+  });
 }
