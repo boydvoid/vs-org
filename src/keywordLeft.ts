dd92e92dba67ebf0537691a6a86c4597a2b11160
Bobby Boyd <bboyd2008@gmail.com>
2018-09-26 22:43:52 -0700

    adding the completed line complete for both left and right

--- src/keywordLeft.ts
+++ src/keywordLeft.ts
@@ -4,7 +4,6 @@ module.exports = function() {
   if (activeTextEditor && activeTextEditor.document.languageId === "vso") {
     const { document } = activeTextEditor;
 
-    let direction = "left";
     let characterArray: any = ["⊖ ", "⊙ ", "⊘ "];
     let position = activeTextEditor.selection.active.line;
     let char: any = characterDecode(characterArray);
@@ -19,71 +18,46 @@ module.exports = function() {
     let nextLine = document.lineAt(position + 1);
     let twoLinesDown = document.lineAt(position + 2);
     if (currentLineText.includes(char)) {
-      let edit = new vscode.WorkspaceEdit();
-      let removeEdit = new vscode.WorkspaceEdit();
-      edit.delete(document.uri, getCurrentLine.range);
 
-      if (direction === "right") {
-        if (currentLineText.includes("DONE")) {
+      if (currentLineText.includes(char)) {
+        let edit = new vscode.WorkspaceEdit();
+        let removeEdit = new vscode.WorkspaceEdit();
+        edit.delete(document.uri, getCurrentLine.range);
+        if (currentLineText.includes("TODO")) {
+          //remove the keyword
           let removedKeyword = formattedText.replace(/\b(DONE|TODO)\b/, "").trim();
           removeEdit.delete(document.uri, getCurrentLine.range);
           removeEdit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + removedKeyword);
-          return vscode.workspace.applyEdit(removeEdit);
-        } else if (!currentLineText.includes("TODO")) {
-          edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO " + formattedText);
-        } else if (!currentLineText.includes("DONE")) {
-          let removeTodo = formattedText.replace(/\b(TODO)\b/, "").trim();
 
+          return vscode.workspace.applyEdit(removeEdit);
+        } else if (!currentLineText.includes("DONE")) {
+          let textToMoveDown = document.getText(
+            new vscode.Range(new vscode.Position(position + 1, 0), new vscode.Position(lastLine.lineNumber, 0))
+          );
+          console.log(textToMoveDown);
+          edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "DONE " + formattedText);
+          //remove the text on the next line
+          edit.delete(document.uri, nextLine.range);
+          //insert the text under the DONE keyword
           edit.insert(
             document.uri,
-            getCurrentLine.range.start,
-            getLeadingSpace + char + "DONE " + "[" + date + "] " + removeTodo
+            new vscode.Position(nextLine.lineNumber, 0),
+            getLeadingSpace + "  " + "COMPLETED: " + "[" + date + "] "
           );
-          // insert new text on current line
-          //text to move down
+
+          edit.replace(
+            document.uri,
+            new vscode.Range(
+              new vscode.Position(twoLinesDown.lineNumber, 0),
+              new vscode.Position(lastLine.lineNumber, 0)
+            ),
+            textToMoveDown
+          );
+        } else if (!currentLineText.includes("TODO")) {
+          let removeDone = formattedText.replace(/\b(DONE)\b/, "").trim();
+          edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO" + " " + removeDone);
         }
         vscode.workspace.applyEdit(edit);
-      } else {
-        if (currentLineText.includes(char)) {
-          let edit = new vscode.WorkspaceEdit();
-          let removeEdit = new vscode.WorkspaceEdit();
-          edit.delete(document.uri, getCurrentLine.range);
-          if (currentLineText.includes("TODO")) {
-            //remove the keyword
-            let removedKeyword = formattedText.replace(/\b(DONE|TODO)\b/, "").trim();
-            removeEdit.delete(document.uri, getCurrentLine.range);
-            removeEdit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + removedKeyword);
-
-            return vscode.workspace.applyEdit(removeEdit);
-          } else if (!currentLineText.includes("DONE")) {
-            let textToMoveDown = document.getText(
-              new vscode.Range(new vscode.Position(position + 1, 0), new vscode.Position(lastLine.lineNumber, 0))
-            );
-            console.log(textToMoveDown);
-            edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "DONE " + formattedText);
-            //remove the text on the next line
-            edit.delete(document.uri, nextLine.range);
-            //insert the text under the DONE keyword
-            edit.insert(
-              document.uri,
-              new vscode.Position(nextLine.lineNumber, 0),
-              getLeadingSpace + " " + "COMPLETED: " + "[" + date + "] "
-            );
-
-            edit.replace(
-              document.uri,
-              new vscode.Range(
-                new vscode.Position(twoLinesDown.lineNumber, 0),
-                new vscode.Position(lastLine.lineNumber, 0)
-              ),
-              textToMoveDown
-            );
-          } else if (!currentLineText.includes("TODO")) {
-            let removeDone = formattedText.replace(/\b(DONE)\b/, "").trim();
-            edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO" + " " + removeDone);
-          }
-          vscode.workspace.applyEdit(edit);
-        }
       }
     }
   }
--- src/keywordRight.ts
+++ src/keywordRight.ts
@@ -4,7 +4,7 @@ module.exports = function() {
   if (activeTextEditor && activeTextEditor.document.languageId === "vso") {
     const { document } = activeTextEditor;
     let characterArray: any = ["⊖ ", "⊙ ", "⊘ "];
-    let direction = "right";
+
     let position = activeTextEditor.selection.active.line;
     let char: any = characterDecode(characterArray);
     let getCurrentLine = document.lineAt(position);
@@ -18,54 +18,47 @@ module.exports = function() {
     let getLeadingSpace = currentLineText.substr(0, currentLineText.indexOf(char));
 
     let date = new Date().toLocaleString();
-
+    let lastLine = document.lineAt(document.lineCount - 1);
+    let nextLine = document.lineAt(position + 1);
+    let twoLinesDown = document.lineAt(position + 2);
     if (currentLineText.includes(char)) {
       let edit = new vscode.WorkspaceEdit();
       let removeEdit = new vscode.WorkspaceEdit();
       edit.delete(document.uri, getCurrentLine.range);
 
-      if (direction === "right") {
-        if (currentLineText.includes("DONE")) {
-          let removedKeyword = formattedText.replace(/\b(DONE|TODO)\b/, "").trim();
-          removeEdit.delete(document.uri, getCurrentLine.range);
-          removeEdit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + removedKeyword);
-          return vscode.workspace.applyEdit(removeEdit);
-        } else if (!currentLineText.includes("TODO")) {
-          edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO " + formattedText);
-        } else if (!currentLineText.includes("DONE")) {
-          let removeTodo = formattedText.replace(/\b(TODO)\b/, "").trim();
-
-          edit.insert(
-            document.uri,
-            getCurrentLine.range.start,
-            getLeadingSpace + char + "DONE " + "[" + date + "] " + removeTodo
-          );
-        }
+      if (currentLineText.includes("DONE")) {
+        let removedKeyword = formattedText.replace(/\b(DONE|TODO)\b/, "").trim();
+        removeEdit.delete(document.uri, getCurrentLine.range);
+        removeEdit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + removedKeyword);
+        return vscode.workspace.applyEdit(removeEdit);
+      } else if (!currentLineText.includes("TODO")) {
+        edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO " + formattedText);
         vscode.workspace.applyEdit(edit);
-      } else {
-        if (currentLineText.includes(char)) {
-          let edit = new vscode.WorkspaceEdit();
-          let removeEdit = new vscode.WorkspaceEdit();
-          edit.delete(document.uri, getCurrentLine.range);
-          if (currentLineText.includes("TODO")) {
-            //remove the keyword
-            let removedKeyword = formattedText.replace(/\b(DONE|TODO)\b/, "").trim();
-            removeEdit.delete(document.uri, getCurrentLine.range);
-            removeEdit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + removedKeyword);
+      } else if (!currentLineText.includes("DONE")) {
+        let removeTodo = formattedText.replace(/\b(TODO)\b/, "").trim();
 
-            return vscode.workspace.applyEdit(removeEdit);
-          } else if (!currentLineText.includes("DONE")) {
-            edit.insert(
-              document.uri,
-              getCurrentLine.range.start,
-              getLeadingSpace + char + "DONE " + "[" + date + "] " + formattedText
-            );
-          } else if (!currentLineText.includes("TODO")) {
-            let removeDone = formattedText.replace(/\b(DONE)\b/, "").trim();
-            edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "TODO" + " " + removeDone);
-          }
-          vscode.workspace.applyEdit(edit);
-        }
+        let textToMoveDown = document.getText(
+          new vscode.Range(new vscode.Position(position + 1, 0), new vscode.Position(lastLine.lineNumber, 0))
+        );
+        edit.insert(document.uri, getCurrentLine.range.start, getLeadingSpace + char + "DONE " + removeTodo);
+        //remove the text on the next line
+        edit.delete(document.uri, nextLine.range);
+        //insert the text under the DONE keyword
+        edit.insert(
+          document.uri,
+          new vscode.Position(nextLine.lineNumber, 0),
+          getLeadingSpace + "  " + "COMPLETED: " + "[" + date + "] "
+        );
+
+        edit.replace(
+          document.uri,
+          new vscode.Range(
+            new vscode.Position(twoLinesDown.lineNumber, 0),
+            new vscode.Position(lastLine.lineNumber, 0)
+          ),
+          textToMoveDown
+        );
+        vscode.workspace.applyEdit(edit);
       }
     }
   }
