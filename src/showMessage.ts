05ab3eb595ce271438f41e88253ed0a2c1dc8701
Bobby Boyd <bboyd2008@gmail.com>
2018-10-08 12:48:56 -0700

    updated class params made bool messages optional

--- src/showMessage.ts
+++ src/showMessage.ts
@@ -8,18 +8,19 @@ export class WindowMessage {
   type: string;
   message: string;
   haveButton: boolean;
-  buttonText: string;
+  buttonText: string | undefined;
   haveUrl: boolean;
-  urlText: string;
+  urlText: string | undefined;
   /**
    * 
-   * @param theType the type of message needs to be Information, Warning, or Error
+   * @param theType the type of message needs to be information, warning, or error
    * @param theMessage the message
    * @param showButton true or false to show a button
-   * @param theButtonText the button text
-   * @param useUrl the url to open in browser
+   * @param useUrl true or false have button open a url
+   * @param theButtonText the button text optional 
+   * @param textForUrl the url text optional
    */
-  constructor(theType: string, theMessage: string, showButton: boolean, theButtonText: string, useUrl: boolean, textForUrl: string) {
+  constructor(theType: string, theMessage: string, showButton: boolean, useUrl: boolean, theButtonText?: string | undefined, textForUrl?: string | undefined) {
     this.type = theType;
     this.message = theMessage;
     this.haveButton = showButton;
@@ -34,7 +35,7 @@ export class WindowMessage {
     if (this.type === "information") {
 
       //open in browswer
-      if (this.haveUrl === true && this.haveButton === true) {
+      if (this.haveUrl === true && this.haveButton === true && this.buttonText !== undefined) {
 
         vscode.window.showInformationMessage(this.message, ...[this.buttonText]).then(selection => {
 
@@ -45,7 +46,7 @@ export class WindowMessage {
       }
     } else if (this.type === "warning") {
       //open in browswer
-      if (this.haveUrl === true && this.haveButton === true) {
+      if (this.haveUrl === true && this.haveButton === true && this.buttonText !== undefined) {
 
         vscode.window.showWarningMessage(this.message, ...[this.buttonText]).then(selection => {
 
@@ -56,7 +57,7 @@ export class WindowMessage {
       }
     } else if (this.type === "error") {
       //open in browswer
-      if (this.haveUrl === true && this.haveButton === true) {
+      if (this.haveUrl === true && this.haveButton === true && this.buttonText !== undefined) {
 
         vscode.window.showErrorMessage(this.message, ...[this.buttonText]).then(selection => {
 
